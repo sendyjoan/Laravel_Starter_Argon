5e170c9134b68de835c38d3b4fbe5ff35fa3dfc3
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2023-01-31 16:27:59 +0700

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'product';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'product_name',
+        'stock',
+        'price',
+        'category_id',
+    ];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+}
